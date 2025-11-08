c5160fd17997974d40529d73472ac817c7e1b94e
rvnsmn <ravenledda69@gmail.com>
2025-11-09 02:30:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-username = input("Enter your username: ")
-
-if len(username) > 12:
-    print("Your username cannot be longer than 12 characters")
-elif not username.find(" ") == -1:
-    print("Your username should not have any space")
-elif not username.isalpha():
-    print("Your username should not have any numbers")
-else:
-    print("Welcome {username}")
-    
+credit_number=input("Enter your credit number: ")
+last_digits = credit_number[-4:]
+print(f"The last four digits of your credit number is XXXX-XXXX-XXXX-{last_digits}")
