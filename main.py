a0a0a3a7d2e1ddecfcb824bb90bf956b47b0d645
rvnsmn <ravenledda69@gmail.com>
2025-11-09 02:38:49 +0800

    PracticeForPrice
    short practice on how to input a proper pricing using python

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-credit_number=input("Enter your credit number: ")
-last_digits = credit_number[-4:]
-print(f"The last four digits of your credit number is XXXX-XXXX-XXXX-{last_digits}")
+price1 = 13990.321
+price2 = -31200.423
+price3 = 44400.555
+
+print(f"Price 1: ${price1:^ ,.2f}")
+print(f"Price 2: ${price2:^ ,.2f}")
+print(f"Price 3: ${price3:^ ,.2f}")
